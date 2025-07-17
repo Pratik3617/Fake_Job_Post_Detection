69d37d81e404fb98a95f9e3119b35bbaaf46ba3d
Prateek <prateek1736@gmail.com>
2025-07-17 03:38:27 +0000

    data validation completed

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -34,3 +34,17 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_REPORT_FILE_NAME: str = "report.yaml"
+
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_REPORT_FILE_NAME: str = "report.yaml"
--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -1,14 +1,16 @@
+from src.components.data_validation import DataValidation
 from src.exception import MyException
 from src.logger import logging
 import sys
 
 from src.components.data_ingestion import DataIngestion
-from src.entity.config_entity import DataIngestionConfig
+from src.entity.config_entity import DataIngestionConfig, DataValidationConfig
 
 
 class TrainingPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
 
     
     def run_pipeline(self) -> None:
@@ -16,6 +18,11 @@ class TrainingPipeline:
             # data ingestion
             data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
             data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-            logging.info("Exited the start_data_ingestion method of TrainPipeline class")
+            logging.info("Exited the data_ingestion method of TrainingPipeline class")
+
+            # data validation
+            data_validation = DataValidation(data_ingestion_artifact, self.data_validation_config)
+            data_validation_artifact = data_validation.initiate_data_validation()
+            logging.info("Exited the data_validation method of TrainingPipeline class")
         except Exception as e:
             raise MyException(e,sys)
